a1ed88c46f93d1ebad71a1d28e578c319b4b1838
fanguangrui <898365387@qq.com>
2020-10-25 17:13:03 +0800

    Gateway网关-通过编码的方式配置路由

---
+++ cloud-gateway-gateway9527/src/main/java/com/fgrapp/cloud/config/GateWayConfig.java
@@ -0,0 +1,39 @@
+package com.fgrapp.cloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * GateWayConfig
+ *
+ * @author fan guang rui
+ * @date 2020年10月25日 17:10
+ */
+@Configuration
+public class GateWayConfig
+{
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder)
+    {
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+
+        routes.route("path_route_guonei",
+                r -> r.path("/guonei")
+                        .uri("http://news.baidu.com/guonei")).build();
+
+        return routes.build();
+    }
+    @Bean
+    public RouteLocator customRouteLocator2(RouteLocatorBuilder routeLocatorBuilder)
+    {
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+
+        routes.route("path_route_guoji",
+                r -> r.path("/guoji")
+                        .uri("http://news.baidu.com/guoji")).build();
+
+        return routes.build();
+    }
+}
