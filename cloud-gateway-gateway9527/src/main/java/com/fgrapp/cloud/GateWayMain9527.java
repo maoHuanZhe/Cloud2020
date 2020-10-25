d5ea0716b54346840dcfe8ea975a7a28f9042448
fanguangrui <898365387@qq.com>
2020-10-25 16:54:08 +0800

    Gateway网关-项目初始化

---
+++ cloud-gateway-gateway9527/src/main/java/com/fgrapp/cloud/GateWayMain9527.java
@@ -0,0 +1,21 @@
+package com.fgrapp.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * GateWayMain9527
+ *
+ * @author fan guang rui
+ * @date 2020年10月25日 16:49
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class,args);
+    }
+
+}
