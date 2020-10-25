37e213bb3b2ea339474a2657846b228c5cc11ede
fanguangrui <898365387@qq.com>
2020-10-25 15:29:42 +0800

    Hystrix服务降级-8001服务降级

--- cloud-provider-hystrix-payment8001/src/main/java/com/fgrapp/cloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/fgrapp/cloud/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.fgrapp.cloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -16,10 +18,17 @@ public class PaymentService {
     {
         return "线程池:  "+Thread.currentThread().getName()+"  paymentInfo_OK,id:  "+id+"\t"+"O(∩_∩)O哈哈~";
     }
-
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler",commandProperties = {
+            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="1000")
+    })
     public String paymentInfo_TimeOut(Integer id)
     {
+//        int age = 10/0;
         try { TimeUnit.MILLISECONDS.sleep(3000); } catch (InterruptedException e) { e.printStackTrace(); }
         return "线程池:  "+Thread.currentThread().getName()+" id:  "+id+"\t"+"O(∩_∩)O哈哈~"+"  耗时(秒): ";
+    }
+    public String paymentInfo_TimeOutHandler(Integer id)
+    {
+        return "线程池:  "+Thread.currentThread().getName()+"  8001系统繁忙或者运行报错，请稍后再试,id:  "+id+"\t"+"o(╥﹏╥)o";
     }
 }
