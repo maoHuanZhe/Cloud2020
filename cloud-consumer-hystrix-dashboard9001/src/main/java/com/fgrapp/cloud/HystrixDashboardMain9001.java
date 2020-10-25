83ae0c71b8baceb358db05f93989ef1c5d78565e
fanguangrui <898365387@qq.com>
2020-10-25 16:27:52 +0800

    Hystrix服务降级-图形化监控

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/fgrapp/cloud/HystrixDashboardMain9001.java
@@ -0,0 +1,21 @@
+package com.fgrapp.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * HystrixDashboardMain9001
+ *
+ * @author fan guang rui
+ * @date 2020年10月25日 16:16
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class,args);
+    }
+
+}
